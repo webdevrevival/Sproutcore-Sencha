4a5d5268c37da7d0ac34c669049bdc5923efffb1
Topher Fangio <fangiotophia@gmail.com>
2012-01-21 00:29:38 -0600

    Initial working integration.

---
+++ apps/touchodo/main.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   Touchodo
+// Copyright: @2012 My Company, Inc.
+// ==========================================================================
+/*globals Touchodo */
+
+sc_require('states/statechart');
+
+Touchodo.main = function main() {
+
+  /*
+   * This function should be super-simple. Just initalize our statechart!
+   */
+  Touchodo.statechart.initStatechart();
+
+};
+
+function main() { Touchodo.main(); }
